7c2d052ead6b7e2e3666a3cc24c0dbe1009d9322
senannetwork <67416841+senannetwork@users.noreply.github.com>
2022-01-16 17:53:54 +0100

    16012022

---
+++ Ecom/admin/includes/temp/footer.php
@@ -0,0 +1,11 @@
+
+
+        <div class="footer">
+            footer
+        </div>
+        <script src="layout\js\all.js"></script>
+        <script src="layout\js\bootstrap.min.js"></script>
+        <script src="layout\js\style.js"></script>
+        
+    </body>
+</html>
---
+++ Ecom/admin/includes/temp/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Admin</title>
+        <link rel="stylesheet" href="..\Admin\layout\css\normalize.css">
+        <link rel="stylesheet" href="..\Admin\layout\css\bootstrap.css">
+        <link rel="stylesheet" href="..\Admin\layout\css\font-awesome.min.css">
+        <link rel="stylesheet" href="..\Admin\layout\css\style.css">
+    </head>
+    <body>
